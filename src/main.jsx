aa5219efe04e4b14d9b0ac3a804a23370d776188
oyeong011 <oyeong011@gmail.com>
2024-05-01 11:10:13 +0900

    [refactor] WA-26 배포 파일 설정

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,18 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './global.css';
 
-async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
+// async function enableMocking() {
+//   if (process.env.NODE_ENV !== 'development') {
+//     return;
+//   }
 
-  const { worker } = await import('./mocks/browser.js');
-  return worker.start();
-}
+//   const { worker } = await import('./mocks/browser.js');
+//   return worker.start();
+// }
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-enableMocking().then(() => {
-  root.render(<App />);
-});
+// enableMocking().then(() => {
+root.render(<App />);
+// });
