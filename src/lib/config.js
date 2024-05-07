06ffee65e646d7a68f09bf838ed25aaedec836d4
oyeong011 <oyeong011@gmail.com>
2024-05-07 18:31:02 +0900

    [refactor] WA-26 배포 사진 경로 변경

--- src/lib/config.js
+++ src/lib/config.js
@@ -16,23 +16,23 @@ export const PWAConfig = {
     display: 'standalone',
     icons: [
       {
-        src: '/public/icon-16x16.png',
+        src: './icon-16x16.png',
         sizes: '16x16',
         type: 'image/png',
       },
       {
-        src: '/public/icon-32x32.png',
+        src: './icon-32x32.png',
         sizes: '32x32',
         type: 'image/png',
       },
       {
-        src: '/public/icon-192x192.png',
+        src: './icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/public/icon-512x512.png',
+        src: './icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
