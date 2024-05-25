710dafc7373592cf08ad4c6151b8cbc83bfd0afe
oyeong011 <oyeong011@gmail.com>
2024-05-25 17:38:29 +0900

    chore: Update axiosRequestConfig in api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
+import https from 'https';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const axiosRequestConfig = {
   baseURL: API_BASE_URL,
   responseType: 'json',
+  // headers: {
+  //   'Content-Type': 'application/json',
+  // },
+  httpsAgent: new https.Agent({ rejectUnauthorized: false }),
 };
 
 const api = axios.create(axiosRequestConfig);
