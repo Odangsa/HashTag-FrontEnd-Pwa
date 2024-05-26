1334472386bd25d350fb7d77018fa869d6a2d0a9
oyeong011 <oyeong011@gmail.com>
2024-05-26 18:48:28 +0900

    [design] WA-30 navbar 디자인 수정

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
       <div>
         <img src="./icon-192x192.png" className="size-24" alt="Logo" />
       </div>
-      <h4 className="font-bold">Waffle</h4>
-      <h4 className="ml-2">Image Uploader</h4>
+      <h4 className="text-2xl font-bold">Waffle</h4>
+      <h4 className="ml-3 text-base font-medium">Image Uploader</h4>
       <p></p>
     </nav>
   );
