703215136efdf85bfc19f7789984dd09075a40d1
oyeong011 <oyeong011@gmail.com>
2024-05-26 21:13:54 +0900

    [fix] WA-30 http환경에서 복사가 안되는 문제 해결

--- src/components/result/CopyListLayout.jsx
+++ src/components/result/CopyListLayout.jsx
@@ -1,7 +1,9 @@
-// Group.js
 import './App.css';
 import useCopyStore from '../../store/useCopyStore';
 import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+toast.configure();
 
 const CopyListLayout = () => {
   const { copyList, deleteCopyList, addHashtag } = useCopyStore();
@@ -9,11 +11,34 @@ const CopyListLayout = () => {
   const handleCopy = async () => {
     const copyTarget = copyList.map((hashtag) => `#${hashtag[1]}`).join(' ');
     console.log('copyTarget: ', copyTarget);
+
+    const fallbackCopyTextToClipboard = (text) => {
+      const textArea = document.createElement('textarea');
+      textArea.value = text;
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+      try {
+        document.execCommand('copy');
+        toast('복사 성공!', { type: 'success' });
+      } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+        toast('복사 실패', { type: 'error' });
+      }
+      document.body.removeChild(textArea);
+    };
+
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(copyTarget);
+      return;
+    }
+
     try {
       await navigator.clipboard.writeText(copyTarget);
       toast('복사 성공!', { type: 'success' });
     } catch (error) {
       console.error('Failed to copy: ', error);
+      toast('복사 실패', { type: 'error' });
     }
   };
 
